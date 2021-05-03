90e03ca0f39e25494f6ac3487285dde1c02d551e
Kimberly <61749817+kimberlyhau@users.noreply.github.com>
2021-05-03 16:13:55 -0400

    Update Program.cs

--- HelloTrill/Program.cs
+++ HelloTrill/Program.cs
@@ -21,20 +21,27 @@ namespace HelloTrill
             {
                 listA.Add(i);                                    // Populate listA with dummy data
             }
-            for (int i = 8; i < 10; i++)
+            for (int i = 5; i < 10; i++)
             {
                 listA.Add(i);                                    // Populate listA with dummy data
             }
 
-            for (int i = 15; i < 20; i++)
+            for (int i = 10; i < 20; i++)
             {
                 listA.Add(i);                                    // Populate listA with dummy data
             }
 
+            double freq1 = 0.5;
+            double freq2 = 0.2;
+            
+            int period1 = Convert.ToInt32( 1/freq1);
+            int period2 = Convert.ToInt32( 1/freq2);
 
-            for (int i = 0; i < 6; i++)
+            int offset = 0;
+            
+            for (int i = 0; i < 10; i++)
             {
-                listB.Add(i*2+1);
+                listB.Add(i*period1+offset);
             }
             
             
@@ -45,13 +52,13 @@ namespace HelloTrill
              */
             var streamA = listA                                 // Creating first stream from listA 
                     .ToObservable()                             // Convert the data list to an Observable first 
-                    .ToTemporalStreamable(e => e, e => e + 1)   // Then convert to Trill temporal stream;
+                    .ToTemporalStreamable(e => e, e => e+1)   // Then convert to Trill temporal stream;
                 ;                                               // nth event in the stream has an integer payload 'n'
                                                                 // and an interval of [n, n+1)
             
             var streamB = listB                                 // Creating streamB (not using yet) similar to streamA.
                     .ToObservable()
-                    .ToTemporalStreamable(e => e, e => e+1)
+                    .ToTemporalStreamable(e => e, e => e+2)
                 ;
             
             /**
@@ -70,6 +77,9 @@ namespace HelloTrill
                 ;                                               // In this case, Adding 1 to each payload using Select
           
             var normalization = streamA
+                    .ShiftEventLifetime(1)
+                    .TumblingWindowLifetime(window_size,0)
+                        /*
                     .Multicast(s=>s
                         .ShiftEventLifetime(1)
                         //.AlterEventLifetime(e => e + 1, 1)
@@ -79,6 +89,7 @@ namespace HelloTrill
                         .Average(e=>e)
                         .Join(s, e=>1, e=>1, (left, right)=> (right-left))
                     )
+                    
                     .Multicast(s=>s
                         .Select (e=> e*e)
                         .ShiftEventLifetime(1)
@@ -89,7 +100,7 @@ namespace HelloTrill
                         .Average(e=>e)
                         .Select(e=>Math.Sqrt(e))
                         .Join(s, e=>1, e=>1, (left, right)=>(right/ left))
-                    )
+                    )*/
                 ;
             var rollingmean = streamA
                     .HoppingWindowLifetime(10, 1)
@@ -134,13 +145,6 @@ namespace HelloTrill
                     .Select((ts, val)=> (ts==val.right)? val.right: val.left)
                 ;
             
-            double freq1 = 0.5;
-            double freq2 = 0.25;
-            
-            int period1 = Convert.ToInt32( 1/freq1);
-            int period2 = Convert.ToInt32( 1/freq2);
-
-            int offset = 1;
             
             var sampling = streamB
             .Multicast(s => s
@@ -149,18 +153,20 @@ namespace HelloTrill
                  
             .AlterEventLifetime(e => e - period1, period1)
                
-            .Chop(0, period2)
+            .Chop(offset, period2)
                
             .Select((ts, val) => val.left + (ts % period1) * (val.right - val.left) / period1)
             .AlterEventDuration(1)
-            .Multicast(s=>
-                 s.Select((ts, val)=>((ts-offset)%period2==0) ? 1:0)
-                 .Where(val=>val ==1)
-                 .Join(s, e=>1, e=>1, (left, right)=> right))
-                       
+            .TumblingWindowLifetime(period2, offset)
+                /*
+                .Multicast(s=>
+                     s.Select((ts, val)=>((ts-offset)%period2==0) ? 1:0)
+                     .Where(val=>val ==1)
+                     .Join(s, e=>1, e=>1, (left, right)=> right))  */
             ;
-
+            
             sampling
+                    
                 .ToStreamEventObservable()                      // Convert back to Observable (of StreamEvents)
                 .Where(e => e.IsData)                           // Only pick data events from the stream
                 .ForEach(e => { Console.WriteLine(e); })        // Print the events to the console
